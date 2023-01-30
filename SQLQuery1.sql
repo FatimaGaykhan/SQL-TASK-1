e7a3cd37c3331ca728d84fbbecf76f0f05d2c4bd
Fatima Gaykhanova <111847102+FatimaGaykhan@users.noreply.github.com>
2023-01-31 00:39:59 +0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,35 @@
+﻿Create Database Company
+ Use Company 
+
+ Create Table Employees
+ (
+ Id int, 
+ Name nvarchar(10),
+ Surname nvarchar(15),
+ Position nvarchar(20), 
+ Salary float
+ )
+
+ Insert into Employees
+ Values 
+ (6,'Elşən','Məmmədov','SMM',1300)
+
+ drop table Employees
+
+select * from Employees
+
+Update Employees set Name='Namiq' where Position='SMM'
+
+Update Employees set Surname='Muradov' where Position='SMM'
+
+Select Min(Salary) from Employees
+
+Select Max(Salary) from Employees
+
+Select AVG(Salary) from Employees
+
+Select * From Employees where Salary > (Select AVG(Salary) from Employees) 
+
+
+
+
